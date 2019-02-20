cd0a7e0028b3d93cb5fd5739d33fe9de5e443b16
Ben <bencwallace@gmail.com>
2019-02-20 03:28:16 +0100

    small changes

--- probly.py
+++ probly.py
@@ -1,50 +1,9 @@
-"""
-Extensions to scipy.stats
-
-Example 1
----------
-Generate a chi-squared distribution from a normal distribution:
-
-    import matplotlib.pyplot as plt
-    import scipy.stats as stats
-    from scirandom import composable
-
-    def square(x):
-        return x ** 2
-    square = composable(square)
-
-    n = stats.norm()
-    chisq = square(n)
-
-    samples = [chisq.rvs() for _ in range(1000)]
-    _ = plt.hist(samples, bins=20)
-    plt.show()
-
-Example 2
----------
-Generate the distribution of the largest eigenvalue of a Wishart matrix.
-
-    import numpy as np
-    import scipy.stats as stats
-    import matplotlib.pyplot as plt
-    from scirandom import composable
-
-    def largest_lambda(w):
-        return np.max(np.linalg.eig(w)[0])
-    largest_lambda = composable(largest_lambda)
-
-    W = stats.wishart(scale=np.eye(2))
-    eig = largest_lambda(W)
-
-    samples = [eig.rvs() for _ in range(1000)]
-    _ = plt.hist(samples, bins=20)
-    plt.show()
-"""
+"""Extensions to scipy.stats"""
 
 
 class sampler():
     """
-    A sampler
+    A sampler.
 
     Basically a very general kind of random variable. Only capable of producing
     random quantities, whose explicit distribution is not necessarily known.
@@ -59,8 +18,8 @@ class sampler():
         return self.f(*samples)
 
 
-def composable(f):
-    """Makes a function act on random variables."""
+def Lift(f):
+    """Lifts a function to the composition map between random variables."""
 
     def F(X):
         return sampler(f, X)
