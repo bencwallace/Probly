6f59766d5d28d0ef48b4bca37b8a8ce66d1a6eb2
Ben <bencwallace@gmail.com>
2019-02-23 19:45:03 +0100

    created abstract random variable

--- probly/__init__.py
+++ probly/__init__.py
@@ -7,22 +7,100 @@ import operator as op
 import random
 
 # Local
-from .programs import _programs
 from .helpers import get_seed, _max_seed
 
 # For helpers
 from functools import wraps
 
 
-class rvarGen(object):
+class rvAbstract(object):
     """
-    A general random variable.
+    An abstract random variable placeholder.
 
-    Can be sampled from and acted upon by appropriate functions decorated with
-    `Lift`.
+    Implements dependency relations and composition map but cannot be sampled
+    on its own.
     """
 
+    # Initialize dependency graph
     graph = nx.MultiDiGraph()
+
+    def __init__(self, f=None, *args):
+        self.graph.add_node(self, method=f)
+        edges = [(self._cast(var), self, {'index': i})
+                 for i, var in enumerate(args)]
+        self.graph.add_edges_from(edges)
+
+    def __call__(self, seed=None):
+        parents = list(self.parents())
+        assert len(parents) > 0, 'Independent abstract random variable'\
+                                 ' cannot be sampled.'
+
+        seed = get_seed(seed)
+
+        # Create {index: parent} dictionary `arguments`
+        data = [self.graph.get_edge_data(p, self) for p in parents]
+        arguments = {}
+        for i in range(len(parents)):
+            indices = [d.values() for d in data[i].values()]
+            for j in range(len(indices)):
+                arguments[data[i][j]['index']] = parents[i]
+
+        # Sample elements of `parents` in order specified by `arguments`
+        # and apply `method` to result
+        samples = [arguments[i](seed) for i in range(len(arguments))]
+        method = self.graph.nodes[self]['method']
+        return method(*samples)
+
+    def parents(self):
+        """Returns list of random variables from which `self` is defined"""
+
+        if self in rvGen.graph:
+            return list(self.graph.predecessors(self))
+        else:
+            return []
+
+    @classmethod
+    def _cast(cls, obj):
+        if isinstance(obj, cls):
+            return obj
+        else:
+            return cls()
+
+    @classmethod
+    def Lift(cls, f):
+        """
+        Lifts a function to the composition map between random variables.
+
+        Args:
+            cls (type): Specifies desired output type of lifted map.
+        """
+
+        @wraps(f)
+        def F(*args):
+            """
+            The lifted function
+
+            Args:
+                `rvar`s and constants
+            """
+
+            # composed_rvar = cls.__new__(cls)
+            # composed_rvar.__init__()
+            F_of_args = cls()
+
+            cls.graph.add_node(F_of_args, method=f)
+            edges = [(cls._cast(var), F_of_args, {'index': i})
+                     for i, var in enumerate(args)]
+            cls.graph.add_edges_from(edges)
+
+            return F_of_args
+
+        return F
+
+
+# Doesn't need to inherit from rvAbstract?
+class rvGen(rvAbstract):
+    """A general (concrete) random variable with a sampling method."""
 
     # Magic
     def __init__(self, sampler=None, origin='random'):
@@ -47,87 +125,29 @@ class rvarGen(object):
             raise TypeError('Unknown origin `{}`'.format(origin))
 
     def __call__(self, seed=None):
+        # Seed as follows for independence of `rvGen`s with same `sampler`
         seed = get_seed(seed)
-        parents = list(self.parents())
-
-        if len(parents) == 0:
-            # Seed as follows for independence of `rvarGen`s with same `sampler`
-            return self.sampler((seed + id(self)) % _max_seed)
-        else:
-            # Create {index: parent} dictionary `arguments`
-            data = [rvarGen.graph.get_edge_data(p, self) for p in parents]
-            arguments = {}
-            for i in range(len(parents)):
-                indices = [d.values() for d in data[i].values()]
-                for j in range(len(indices)):
-                    arguments[data[i][j]['index']] = parents[i]
-
-            # Sample elements of `parents` in order specified by `arguments`
-            # and apply `method` to result
-            samples = [arguments[i](seed) for i in range(len(arguments))]
-            method = rvarGen.graph.nodes[self]['method']
-            return method(*samples)
+        return self.sampler((seed + id(self)) % _max_seed)
 
     # Instance methods
     def copy(self, obj):
-        """Return a random variable with the same distribution as `self`"""
+        """Return an independent copy."""
 
-        # Shallow copy is ok as `rvarGen` isn't mutable
+        # Shallow copy is ok as `rvGen` isn't mutable
         return copy.copy(self)
-
-    def parents(self):
-        """Returns list of random variables from which `self` is defined"""
-
-        if self in rvarGen.graph:
-            return list(rvarGen.graph.predecessors(self))
-        else:
-            return []
 
     # Class methods
     @classmethod
-    def _compose(cls, f, *args):
-        composed_rvar = cls.__new__(cls)
-
-        cls.graph.add_node(composed_rvar, method=f)
-        edges = [(cls._cast(var), composed_rvar, {'index': i})
-                 for i, var in enumerate(args)]
-        cls.graph.add_edges_from(edges)
-
-        return composed_rvar
-
-    @classmethod
     def _cast(cls, obj):
-        """Cast constants to `rvarGen` objects."""
+        """Cast constants to `rvGen` objects."""
 
         if isinstance(obj, cls):
             return obj
         else:
             return cls(lambda seed=None: obj)
 
-    @classmethod
-    def Lift(cls, f):
-        """
-        Lifts a function to the composition map between random variables.
 
-        Args:
-            cls (type): Specifies desired output type of lifted map.
-        """
-
-        @wraps(f)
-        def F(*args):
-            """
-            The lifted function
-
-            Args:
-                `rvar`s and constants
-            """
-
-            return cls._compose(f, *args)
-
-        return F
-
-
-class rvarNumeric(rvarGen):
+class rvarNumeric(rvGen):
     """
     A random variable of numeric type. Not for direct use.
 
@@ -137,125 +157,125 @@ class rvarNumeric(rvarGen):
     # Is there a more natural way to inherit the Lift decorator?
 
     # Left commuting binary operators
-    @rvarGen.Lift
+    @rvGen.Lift
     def __add__(self, x):
         return op.add(self, x)
 
-    @rvarGen.Lift
+    @rvGen.Lift
     def __sub__(self, x):
         return op.sub(self, x)
 
-    @rvarGen.Lift
+    @rvGen.Lift
     def __mul__(self, x):
         return op.mul(self, x)
 
     # Right commuting binary operators
-    @rvarGen.Lift
+    @rvGen.Lift
     def __radd__(self, x):
         return x.__add__(self)
 
-    @rvarGen.Lift
+    @rvGen.Lift
     def __rsub__(self, x):
         return x.__sub__(self)
 
-    @rvarGen.Lift
+    @rvGen.Lift
     def __rmul__(self, x):
         return x.__mul__(self)
 
     # Left non-commuting binary operators
-    @rvarGen.Lift
+    @rvGen.Lift
     def __matmul__(self, x):
         return op.matmul(self, x)
 
-    @rvarGen.Lift
+    @rvGen.Lift
     def __truediv__(self, x):
         return op.truediv(self, x)
 
-    @rvarGen.Lift
+    @rvGen.Lift
     def __floordiv__(self, x):
         return op.floordiv(self, x)
 
-    @rvarGen.Lift
+    @rvGen.Lift
     def __mod__(self, x):
         return op.mod(self, x)
 
-    @rvarGen.Lift
+    @rvGen.Lift
     def __divmod__(self, x):
         return op.divmod(self, x)
 
-    @rvarGen.Lift
+    @rvGen.Lift
     def __pow__(self, x):
         return op.pow(self, x)
 
     # Right non-commuting binary operators
-    @rvarGen.Lift
+    @rvGen.Lift
     def __rmatmul__(self, x):
-        x = rvarGen._cast(x)
+        x = rvGen._cast(x)
         return x.__matmul__(self)
 
-    @rvarGen.Lift
+    @rvGen.Lift
     def __rtruediv__(self, x):
-        x = rvarGen._cast(x)
+        x = rvGen._cast(x)
         return x.__truediv__(self)
 
-    @rvarGen.Lift
+    @rvGen.Lift
     def __rfloordiv__(self, x):
-        x = rvarGen._cast(x)
+        x = rvGen._cast(x)
         return x.__floordiv__(self)
 
-    @rvarGen.Lift
+    @rvGen.Lift
     def __rmod__(self, x):
-        x = rvarGen._cast(x)
+        x = rvGen._cast(x)
         return x.__mod__(self)
 
-    @rvarGen.Lift
+    @rvGen.Lift
     def __rdivmod__(self, x):
-        x = rvarGen._cast(x)
+        x = rvGen._cast(x)
         return x.__divmod__(self)
 
-    @rvarGen.Lift
+    @rvGen.Lift
     def __rpow__(self, x):
-        x = rvarGen._cast(x)
+        x = rvGen._cast(x)
         return x.__pow__(self)
 
     # Unary operators
-    @rvarGen.Lift
+    @rvGen.Lift
     def __neg__(self):
         return op.neg(self)
 
-    @rvarGen.Lift
+    @rvGen.Lift
     def __pos__(self):
         return op.pos(self)
 
-    @rvarGen.Lift
+    @rvGen.Lift
     def __abs__(self):
         return op.abs(self)
 
-    @rvarGen.Lift
+    @rvGen.Lift
     def __complex__(self):
         return op.complex(self)
 
-    @rvarGen.Lift
+    @rvGen.Lift
     def __int__(self):
         return op.int(self)
 
-    @rvarGen.Lift
+    @rvGen.Lift
     def __float__(self):
         return op.float(self)
 
-    @rvarGen.Lift
+    @rvGen.Lift
     def __round__(self):
         return op.round(self)
 
-    @rvarGen.Lift
+    @rvGen.Lift
     def __trunc__(self):
         return op.trunc(self)
 
-    @rvarGen.Lift
+    @rvGen.Lift
     def __floor__(self):
         return op.floor(self)
 
-    @rvarGen.Lift
+    @rvGen.Lift
     def __ceil__(self):
         return op.ceil(self)
 
@@ -275,7 +295,7 @@ class rvar(rvarNumeric):
 
 #     # circular:
 #     def __new__(cls, arr):
-#         arr = np.array([rvarGen._cast(var) for var in arr])
+#         arr = np.array([rvGen._cast(var) for var in arr])
 
 #         @cls.Lift
 #         def make_array(*args):
@@ -287,7 +307,7 @@ class rvar(rvarNumeric):
 #     def __getitem__(self, key):
 #         assert hasattr(self(0), '__getitem__'),\
 #             'Scalar {} object not subscriptable'.format(self.__class__)
-#         return rvarGen._getitem(self, key)
+#         return rvGen._getitem(self, key)
 
 #     # To do: add matrix operations
 
@@ -298,7 +318,7 @@ class rvar(rvarNumeric):
 #         if isinstance(obj, cls):
 #             return obj
 #         else:
-#             return rvarGen.array(obj)
+#             return rvGen.array(obj)
 
 #     @classmethod
 #     def _getitem(cls, obj, key):
