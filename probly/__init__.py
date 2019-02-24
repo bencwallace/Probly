2036a6951f9fce7608927148589249966510e66c
Ben <bencwallace@gmail.com>
2019-02-24 20:12:59 +0100

    got rid of execs

--- probly/__init__.py
+++ probly/__init__.py
@@ -6,7 +6,7 @@ import numpy as np
 import operator as op
 from functools import wraps
 
-from .programs import _programs
+# from .programs import _programs
 from .helpers import get_seed, _max_seed
 
 
@@ -27,7 +27,127 @@ def Lift(f):
     return F
 
 
-class rv(object):
+class Numeric(object):
+    # Matrix operators
+    @Lift
+    def __matmul__(self, x):
+        return op.matmul(self, x)
+
+    def __rmatmul__(self, x):
+        X = self._cast(x)
+        return X.__matmul__(self)
+
+    # Left commuting binary operators
+    @Lift
+    def __add__(self, x):
+        return op.add(self, x)
+
+    @Lift
+    def __sub__(self, x):
+        return op.sub(self, x)
+
+    @Lift
+    def __mul__(self, x):
+        return op.mul(self, x)
+
+    # Right commuting binary operators
+    def __radd__(self, x):
+        X = self._cast(x)
+        return X.__add__(self)
+
+    def __rsub__(self, x):
+        X = self._cast(x)
+        return X.__sub__(self)
+
+    def __rmul__(self, x):
+        X = self._cast(x)
+        return X.__mul__(self)
+
+    # Left non-commuting binary operators
+    @Lift
+    def __truediv__(self, x):
+        return op.truediv(self, x)
+
+    @Lift
+    def __floordiv__(self, x):
+        return op.floordiv(self, x)
+
+    @Lift
+    def __mod__(self, x):
+        return op.mod(self, x)
+
+    @Lift
+    def __divmod__(self, x):
+        return op.divmod(self, x)
+
+    @Lift
+    def __pow__(self, x):
+        return op.pow(self, x)
+
+    # Right non-commuting binary operators
+    def __rtruediv__(self, x):
+        X = self._cast(x)
+        return X.__truediv__(self)
+
+    def __rfloordiv__(self, x):
+        X = self._cast(x)
+        return X.__floordiv__(self)
+
+    def __rmod__(self, x):
+        X = self._cast(x)
+        return X.__mod__(self)
+
+    def __rdivmod__(self, x):
+        X = self._cast(x)
+        return X.__divmod__(self)
+
+    def __rpow__(self, x):
+        X = self._cast(x)
+        return X.__pow__(self)
+
+    # Unary operators
+    @Lift
+    def __neg__(self):
+        return op.neg(self)
+
+    @Lift
+    def __pos__(self):
+        return op.pos(self)
+
+    @Lift
+    def __abs__(self):
+        return op.abs(self)
+
+    @Lift
+    def __complex__(self):
+        return op.complex(self)
+
+    @Lift
+    def __int__(self):
+        return op.int(self)
+
+    @Lift
+    def __float__(self):
+        return op.float(self)
+
+    @Lift
+    def __round__(self):
+        return op.round(self)
+
+    @Lift
+    def __trunc__(self):
+        return op.trunc(self)
+
+    @Lift
+    def __floor__(self):
+        return op.floor(self)
+
+    @Lift
+    def __ceil__(self):
+        return op.ceil(self)
+
+
+class rv(Numeric):
     """
     A random variable placeholder.
 
@@ -78,10 +198,6 @@ class rv(object):
         assert hasattr(self(0), '__getitem__'),\
             'Scalar {} object not subscriptable'.format(self.__class__)
         return rv._getitem(self, key)
-
-    # Define operators for emulating numeric types
-    for p in _programs:
-        exec(p)
 
     def parents(self):
         """Returns list of random variables from which `self` is defined"""
