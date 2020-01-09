b4f97ef1a47a79da2e3116501879f16b0ade666c
Ben <bencwallace@gmail.com>
2020-01-09 18:43:12 +0100

    small changes

--- probly/distr/distributions.py
+++ probly/distr/distributions.py
@@ -5,18 +5,14 @@ Random variables following common distributions.
 from ..core.random_variables import RandomVariable, RandomVariableWithIndependence
 
 
+# ultimately makes Distribution an instance of a specialization of the reader monad
+# metaclasses needed so that subclasses also become instances
 class Lift(type):
     def __call__(cls, *rvs, **kwargs):
         if any((isinstance(rv, RandomVariable) for rv in rvs)):
             return RandomDistribution(cls, *rvs)
         else:
             return super().__call__(*rvs, **kwargs)
-
-
-class Distribution(RandomVariableWithIndependence, metaclass=Lift):
-    def __init__(self):
-        op = self._sampler
-        super().__init__(op)
 
 
 class RandomDistribution(RandomVariable):
@@ -28,3 +24,9 @@ class RandomDistribution(RandomVariable):
     def _sampler(self, seed=None):
         seed = self._get_seed(seed)
         return self.distr(*(rv(seed) for rv in self.rvs))(seed)
+
+
+class Distribution(RandomVariableWithIndependence, metaclass=Lift):
+    def __init__(self):
+        op = self._sampler
+        super().__init__(op)
